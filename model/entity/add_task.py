cc1b3e6b933389d9046245cfb7ee482c563f706b
Maedeh <Maedeh.Gholami24@gmail.com>
2024-08-15 16:36:45 +0330

    update

--- model/entity/add_task.py
+++ model/entity/add_task.py
@@ -19,3 +19,24 @@ scrollbar_task.config(command=listbox_task.yview)
 entry_button=Button(window,text="Add task",width= 50,command=entertask)
 entry_button.pack(pady=3)
 
+delete_button=Button(window,text="Delete task",width=50,command=deletetask)
+delete_button.pack(pady=3)
+
+mark_button=Button(window,text="Mark as completed",width=50,command=markcompleted)
+mark_button.pack(pady=3)
+
+window.mainloop()
+def entertask():
+    input_text=""
+    def add():
+        input_text=entry_task.get(1.0,"end_1c")
+        if input_text=="":
+            tkinter.messagebox(title="Warning!",message="Please enter some text")
+        else:
+            listbox_task.insert(END,input_text)
+            root1.destroy
+    root1=Tk()
+    root1.title("Add task")
+    entry_task=Text(root1,width=40,height=4)
+    entry_task.pack()
+    button_temp=Button(root1,text="Add task")
